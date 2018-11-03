ba999f15945edeb41242e4d92c8a733836875ca9
kimberlymciver <kimberlyrmciver@gmail.com>
2018-11-03 16:39:21 -0700

    created sign in view controller

---
+++ Pods/Target Support Files/Pods-howtosoftwareengineer/Pods-howtosoftwareengineer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_howtosoftwareengineer : NSObject
+@end
+@implementation PodsDummy_Pods_howtosoftwareengineer
+@end
---
+++ howtosoftwareengineer/SignInViewController.swift
@@ -0,0 +1,97 @@
+//
+//  SignInViewController.swift
+//  howtosoftwareengineer
+//
+//  Created by Kim McIver on 10/25/18.
+//  Copyright © 2018 Kimberly McIver. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+
+   @IBOutlet weak var dataLabel: UILabel!
+   var dataObject: String = ""
+
+
+    @IBOutlet weak var signInSelector: UISegmentedControl!
+    @IBOutlet weak var welcomeBackLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgotPassworButton: UIButton!
+    @IBOutlet weak var continueAsGuestButton: UIButton!
+    
+    var isSignIn: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+    @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
+        
+        //Flip the boolean
+        isSignIn = !isSignIn
+        
+        // Check the boolean and set the buttons and labels
+        if isSignIn {
+          welcomeBackLabel.text = "Welcome Back"
+            logInButton.setTitle("Log In", for: .normal)
+        } else {
+            welcomeBackLabel.text = "Register"
+            logInButton.setTitle("Register", for: .normal)
+        }
+    }
+    
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+        
+        // TODO: form validation on email and password
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+            
+            // Check if it's log in or register
+            if isSignIn {
+                // Sign in the user with Firebase
+                Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                    
+                    // Check that user isn't nil
+                    if let u = user {
+                        // user is found, go to main screen
+                        self.performSegue(withIdentifier: "goToMainScreen", sender: self)
+                    } else {
+                        // Error: check error and show message
+                    }
+                }
+                
+            } else {
+                // Register the user with Firebase
+                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                    // Check that user isn't nil
+                    if let u = user {
+                        // User is found, go to main screen
+                        self.performSegue(withIdentifier: "goToMainScreen", sender: self)
+                    } else {
+                        // Error: check error and show message
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func forgotPassButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func continueAsGuestButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
