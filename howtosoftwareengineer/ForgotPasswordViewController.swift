d39fba135423e0c67a7d9200b7dd9e61d280da70
kimberlymciver <kimberlyrmciver@gmail.com>
2018-11-23 20:34:27 -0800

    added forgot password view controller

--- howtosoftwareengineer/ForgotPasswordViewController.swift
+++ howtosoftwareengineer/ForgotPasswordViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class ForgotPasswordViewController: UIViewController {
 
     @IBOutlet weak var forgotPasswordLabel: UILabel!
+    
     @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var resetPasswordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+         forgotPasswordLabel.numberOfLines = 2
         // Do any additional setup after loading the view.
     }
-   
-    @IBAction func backButtonTapped(_ sender: UIButton) {
+    
+    
+    @IBAction func backButtonTapped() {
         self.performSegue(withIdentifier: "goToSignIn", sender: self)
     }
-
+    
 }
